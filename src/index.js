1749c7bdf8c45206f85095e1905eb8e56bf2b4e4
eoingmelly <eoin.melly@eisystems.ie>
2021-01-07 21:51:25 +0000

    Multi components nested, with Explicit and Implicit arrow functions used

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDom from 'react-dom';
-
-
+import React from "react";
+import ReactDom from "react-dom";
 
 function Greeting() {
-  return <h4>this is Eoin and this is my first component</h4>;
+  return (
+    <div>
+      <Person />
+      <Message />
+      <Message />
+      <Message />
+      <Message />
+    </div>
+  );
 }
 
-ReactDom.render(<Greeting />, document.getElementById('root'))
+const Person = () => <h2>John Doe</h2>;
+
+const Message = () => {
+  return <p>This is my message</p>;
+};
+
+ReactDom.render(<Greeting />, document.getElementById("root"));
