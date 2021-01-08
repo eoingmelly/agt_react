f06875417cd4af8fb282a5b75a33fb026ff5809c
eoingmelly <eoin.melly@eisystems.ie>
2021-01-08 17:32:54 +0000

    Destructuring object in function before populating the 'book'

--- src/index.js
+++ src/index.js
@@ -4,32 +4,40 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
-const author = "Frank Worral";
-const title = "Rory McIlroy - The Champion Golfer";
-const img = "https://m.media-amazon.com/images/I/91NFL1efZNL._AC_UL320_.jpg";
+// const author = "Frank Worral";
+// const title = "Rory McIlroy - The Champion Golfer";
+// const img = "https://m.media-amazon.com/images/I/91NFL1efZNL._AC_UL320_.jpg";
+
+//Example as multiple Book Objects...
+const bookOne = {
+  img: "https://m.media-amazon.com/images/I/91NFL1efZNL._AC_UL320_.jpg",
+  title: "Rory McIlroy - The Champion Golfer",
+  author: "Frank Worral",
+};
+
+const bookTwo = {
+  img: "https://m.media-amazon.com/images/I/41IA0aUwZnL._AC_UY218_.jpg",
+  title: "Rory McIlroy - The Biography Updated",
+  author: "Frank Worral",
+};
 
 function BookList() {
   return (
     <section className="booklist">
-      <Book job="developer" />
-      <Book job="banker" number={77639} price="€56.00" />
-      {/* And an empty one below... */}
-      <Book />
+      <Book book={bookOne} />
+      <Book book={bookTwo} />
     </section>
   );
 }
 
-const Book = (props) => {
+const Book = ({ book }) => {
   //named props by convention. contains empty object unless supplied with property definitions in KeyValue pairs as above.
-  console.log(props);
+  let { img, title, author } = book;
   return (
     <article className="book">
-      <img src={img} alt="Rory McIlroy - The Champion Golfer" />
+      <img src={img} alt="Rory McIlroy " />
       <h2>{title}</h2>
-      <h4>
-        {/* Example of using the value of props as defined above in KVP  */}
-        by {author} ({props.job})
-      </h4>
+      <h4>by {author}</h4>
     </article>
   );
 };
