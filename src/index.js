668341db3552f9b223648e101d9f64dd186add08
eoingmelly <eoin.melly@eisystems.ie>
2021-01-08 15:06:57 +0000

    Props passed into components

--- src/index.js
+++ src/index.js
@@ -4,23 +4,27 @@ import ReactDom from "react-dom";
 //CSS
 import "./index.css";
 
+const author = "Frank Worral";
+const title = "Rory McIlroy - The Champion Golfer";
+const img = "https://m.media-amazon.com/images/I/91NFL1efZNL._AC_UL320_.jpg";
+
 function BookList() {
   return (
     <section className="booklist">
+      <Book job="developer" />
+      <Book number={77639} price="€56.00" />
+      {/* And an empty one below... */}
       <Book />
     </section>
   );
 }
 
-const author = "Frank Worral";
-const Book = () => {
-  const title = "Rory McIlroy - The Champion Golfer";
+const Book = (props) => {
+  //named props by convention. contains empty object unless supplied with property definitions in KeyValue pairs as above.
+  console.log(props);
   return (
     <article className="book">
-      <img
-        src="https://m.media-amazon.com/images/I/91NFL1efZNL._AC_UL320_.jpg"
-        alt="Rory McIlroy - The Champion Golfer"
-      />
+      <img src={img} alt="Rory McIlroy - The Champion Golfer" />
       <h2>{title}</h2>
       <h4>by {author}</h4>
     </article>
